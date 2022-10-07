3c9d2076d676bfd4a1da4389b41b203bc5f68e3e
iali <engr.imran_1242@hotmail.com>
2022-10-07 16:24:07 +0200

    starting whatsapp clone

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  width: 100%;
+  background: red;
+`
+
+function App() {
+  return <Container>Imran ali</Container>
+}
+
+export default App
